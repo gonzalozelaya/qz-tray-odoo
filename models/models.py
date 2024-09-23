4d1b1f41734b5c59dda180ed9aaa04bc39f3d4e6
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2024-09-23 16:21:38 -0300

    Added Stock lot to print

--- models/models.py
+++ models/models.py
@@ -15,6 +15,19 @@ class ProductTemplate(models.Model):
             'target': 'new',
             'url': self.print_url + res_id,
         }
+        
+class StockLot(models.Model):
+    _name = 'stock.lot'
+    _inherit = ['stock.lot','qz.tray']
+
+    def print_label(self):
+        self.zpl_code =html.unescape(f"^XA^CF0,30^FO30,30^A0N,20,20^FB360,3,,^FD{html.unescape(self.display_name)}^FS^FO90,100^BY2^BCN,60,,,A^FD12345678^FS^FO320,200^A0N,30,30^FD${self.list_price}^FS^FO460,30^A0N,20,20^FB360,3,,^FD{html.unescape(self.display_name)}^FS^FO490,100^BY2^BCN,60,,,A^FD12345678^FS^FO720,200^A0N,30,30^FD{self.list_price}^FS^XZ")
+        res_id = '?res_id=%s&res_model=product.template'%(self.id)
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'new',
+            'url': self.print_url + res_id,
+        }
 
 class ZPLReport(models.Model):
     _name = 'zpl.report.template'
